5d88657487aad2c4d3bc3c89fd9e358c320bf5ec
ksmith4134 <73805794+ksmith4134@users.noreply.github.com>
2022-04-12 16:40:25 -0400

    Added links to GitHub repos on homepage

--- components/Home/Deployment.js
+++ components/Home/Deployment.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { faFolder, faArrowTurnDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -19,10 +20,13 @@ export default function Deployment() {
               <FontAwesomeIcon icon={faFolder} className="text-4xl text-[#ffca58]" />
               <h3 className="text-[#000000] text-2xl pl-3">Frontend Files</h3>
             </div>
-            <div className="h-full lg:h-3/4 text-center card p-12 mt-6">
-              <div className="">
+            <div className="h-full lg:h-3/4 text-center card px-12 py-16 mt-6">
+              <div>
                 <Image src="/vercel.svg" width={202.5} height={45} />
-                <p className="mt-6">The frontend of this site consists of a NEXT.js project styled using Tailwind CSS. These files are deployed directly from GitHub using Vercel's CDN network.</p>
+                <p className="mt-8">The frontend of this site consists of a NEXT.js project styled using Tailwind CSS. These files are deployed directly from GitHub using Vercel's CDN network.</p>
+              </div>
+              <div className="mt-6 text-blue-500 underline underline-offset-2">
+                <Link href="https://github.com/ksmith4134/nextjs-frontend"><a target="_blank">GitHub Repository</a></Link>
               </div>
             </div>
           </div>
@@ -32,13 +36,16 @@ export default function Deployment() {
               <FontAwesomeIcon icon={faFolder} className="text-4xl text-[#ffca58]" />
               <h3 className="text-[#000000] text-2xl pl-3">Backend Files</h3>
             </div>
-            <div className="h-full lg:h-3/4 text-center card p-12 mt-6">
+            <div className="h-full lg:h-3/4 text-center card px-12 py-16 mt-6">
               <div className="flex place-items-center justify-center">
                 <Image src="/heroku.svg" width={160.5} height={45} />
                 <div className="px-4 text-2xl">+</div>
                 <Image src="/postgresql.svg" width={45} height={45} />
               </div>
-              <p className="mt-6">The backend of this site is deployed using Heroku and PostgreSQL. Content is managed using Strapi (headless CMS) and Cloudinary image hosting.</p>
+              <p className="mt-8">The backend of this site is deployed using Heroku and PostgreSQL. Content is managed using Strapi (headless CMS) and Cloudinary image hosting.</p>
+              <div className="mt-6 text-blue-500 underline underline-offset-2">
+                <Link href="https://github.com/ksmith4134/strapi-headless-cms"><a target="_blank">GitHub Repository</a></Link>
+              </div>
             </div>
           </div>
 
